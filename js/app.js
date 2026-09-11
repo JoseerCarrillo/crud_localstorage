578e1cd3733ac658b68ecceff40492a71eac4c12
rubendavidallo <rubendavidallo@ufps.edu.co>
2026-09-11 06:54:47 -0500

    Capturar el formulario con JavaScript, capturar valores y crear objeto estudiante

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ formulario.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("Formulario enviado");
 })
+const nombre = document.querySelector("#nombre").value;
+const correo = document.querySelector("#correo").value;
+const programa = document.querySelector("#programa").value;
+
+const estudiante = {
+    nombre,
+    correo,
+    programa
+};
