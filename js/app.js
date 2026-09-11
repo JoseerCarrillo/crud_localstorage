8b03cb867d64e1efde6cf7d61417fd7364d940f8
rubendavidallo <rubendavidallo@ufps.edu.co>
2026-09-11 07:08:15 -0500

    Mostrar estudiantes en la tabla

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
+let estudiantes = [];
+
 const formulario = document.querySelector("#formEstudiante");
 
 formulario.addEventListener("submit", function(event) {
     event.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    const programa = document.querySelector("#programa").value;
+
+    const estudiante = {
+        nombre,
+        correo,
+        programa
+    };
+    estudiantes.push(estudiante);
+    mostrarEstudiantes();
+    formulario.reset();
     console.log("Formulario enviado");
 })
-const nombre = document.querySelector("#nombre").value;
-const correo = document.querySelector("#correo").value;
-const programa = document.querySelector("#programa").value;
 
-const estudiante = {
-    nombre,
-    correo,
-    programa
-};
+function mostrarEstudiantes(lista = estudiantes) {
+
+    const tabla = document.querySelector("#tablaEstudiantes");
+
+    tabla.innerHTML = "";
+
+    lista.forEach(estudiante => {
+
+        tabla.innerHTML += `
+            <tr>
+                <td>${estudiante.nombre}</td>
+                <td>${estudiante.correo}</td>
+                <td>${estudiante.programa}</td>
+                <td>Acciones</td>
+            </tr>
+        `;
+    });
+}
